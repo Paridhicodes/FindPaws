ad68c7acff17e6328d2e04780aee94b90f803d8b
Paridhi Jain <paridhijain0201@gmail.com>
2022-05-15 02:08:22 +0530

    Firebase Storage Done

--- lib/screens/InitialSetup/UploadImage.dart
+++ lib/screens/InitialSetup/UploadImage.dart
@@ -1,4 +1,5 @@
 import 'package:find_paws_engage/screens/InitialSetup/questions/question1.dart';
+import 'package:find_paws_engage/storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:find_paws_engage/constants.dart';
@@ -10,9 +11,14 @@ import 'package:image_picker/image_picker.dart';
 import 'package:find_paws_engage/components/AppBarInit.dart';
 import 'dart:io';
 import 'dart:async';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:file_picker/file_picker.dart';
 
 class UploadImage extends StatefulWidget {
   static const String id = "upload_image";
+
   const UploadImage({Key? key}) : super(key: key);
 
   @override
@@ -20,22 +26,26 @@ class UploadImage extends StatefulWidget {
 }
 
 class _UploadImageState extends State<UploadImage> {
-  var imageFile;
+  final _firebaseStorage = FirebaseStorage.instance;
+  Storage storage = Storage();
+  var imagePath;
+  var imageName;
+  String imageUrl = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            AppBarInit(),
+            const AppBarInit(),
             Container(
-              child: imageFile == null
+              child: imagePath == null
                   ? Container(
                       child: Column(
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: const ListTile(
+                          const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: ListTile(
                               title: Text(
                                 "Add a cute photo of your pet!",
                                 style: TextStyle(
@@ -50,7 +60,7 @@ class _UploadImageState extends State<UploadImage> {
                               ),
                             ),
                           ),
-                          SizedBox(
+                          const SizedBox(
                             height: 30,
                           ),
                           Image.asset(
@@ -74,17 +84,17 @@ class _UploadImageState extends State<UploadImage> {
                                   ),
                                 ),
                               ),
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: RoundedButton(
-                                    buttonText: 'From Camera',
-                                    onPressed: () {
-                                      _getFromCamera();
-                                    },
-                                  ),
-                                ),
-                              ),
+                              // Expanded(
+                              //   child: Padding(
+                              //     padding: const EdgeInsets.all(8s.0),
+                              //     child: RoundedButton(
+                              //       buttonText: 'From Camera',
+                              //       onPressed: () {
+                              //         _getFromCamera();
+                              //       },
+                              //     ),
+                              //   ),
+                              // ),
                             ],
                           )
                         ],
@@ -116,18 +126,16 @@ class _UploadImageState extends State<UploadImage> {
                             decoration: BoxDecoration(
                               border: Border.all(color: mainColor, width: 10),
                             ),
-                            child: Image.file(imageFile),
+                            child: Image.file(File(imagePath)),
                           ),
-                          // Container(
-                          //   child: Image.file(
-                          //     imageFile,
-                          //     fit: BoxFit.cover,
-                          //   ),
-                          // ),
                           RoundedButton(
                             buttonText: 'Next',
-                            onPressed: () {
-                              Navigator.pushNamed(context, Question1.id);
+                            onPressed: () async {
+                              storage.uploadFile(imagePath, imageName);
+                              var downurl =
+                                  await storage.downloadUrl(imageName);
+                              print(downurl);
+                              await Navigator.pushNamed(context, Question1.id);
                             },
                           )
                         ],
@@ -142,31 +150,47 @@ class _UploadImageState extends State<UploadImage> {
 
   /// Get from gallery
   _getFromGallery() async {
-    PickedFile? pickedFile = await ImagePicker().getImage(
-      source: ImageSource.gallery,
-      maxWidth: 1800,
-      maxHeight: 1800,
+    // PickedFile? pickedFile = await ImagePicker().getImage(
+    //   source: ImageSource.gallery,
+    //   maxWidth: 1800,
+    //   maxHeight: 1800,
+    // );
+    // if (pickedFile != null) {
+    //   setState(() {
+    //     imageFile = File(pickedFile.path);
+    //     imageName = imageFile.split('/').last;
+    //   });
+    // }
+    final results = await FilePicker.platform.pickFiles(
+      allowMultiple: false,
+      type: FileType.custom,
+      allowedExtensions: ['png', 'jpg'],
     );
-    if (pickedFile != null) {
+
+    if (results != null) {
       setState(() {
-        imageFile = File(pickedFile.path);
-        print(imageFile);
+        imagePath = results.files.single.path;
+        imageName = results.files.single.name;
+        print(imageName);
+        print(imagePath);
       });
     }
   }
 
   /// Get from Camera
-  _getFromCamera() async {
-    PickedFile? pickedFile = await ImagePicker().getImage(
-      source: ImageSource.camera,
-      maxWidth: 1800,
-      maxHeight: 1800,
-    );
-    if (pickedFile != null) {
-      setState(() {
-        imageFile = File(pickedFile.path);
-        print(imageFile);
-      });
-    }
-  }
+  // _getFromCamera() async {
+  //   await Permission.photos.request();
+  //   PickedFile? pickedFile = await ImagePicker().getImage(
+  //     source: ImageSource.camera,
+  //     maxWidth: 1800,
+  //     maxHeight: 1800,
+  //   );
+  //   if (pickedFile != null) {
+  //     setState(() {
+  //       imageFile = File(pickedFile.path);
+  //
+  //       print(imageFile);
+  //     });
+  //   }
+  // }
 }
---
+++ lib/storage.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+
+class Storage {
+  final firebase_storage = FirebaseStorage.instance;
+
+  Future<void> uploadFile(String filePath, String fileName) async {
+    File file = File(filePath);
+
+    try {
+      await firebase_storage.ref('test/$fileName').putFile(file);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<String> downloadUrl(String imageName) async {
+    String downloadUrl =
+        await firebase_storage.ref('test/$imageName').getDownloadURL();
+    return downloadUrl;
+  }
+}
